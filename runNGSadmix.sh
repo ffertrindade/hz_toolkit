4767c1d7d3ad61c807b2b22fa02499a3d0a25580
ffertrindade <70981641+ffertrindade@users.noreply.github.com>
2020-09-11 16:06:52 -0300

    Add files via upload
    Initial versions of some scripts.

---
+++ runNGSadmix.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# rodar varios NGSadmix
+
+# beagle de entrada
+input=~/angsd/calling/run003_gl_batch001.filtered.beagle.gz
+output=~/ngsadmix/batch001_run003_filtered
+
+# corridas
+echo "##### k2... #####"
+NGSadmix -likes $input -K 2 -P 10 -minMaf 0.05 -seed 1 -o $output\_k2
+
+echo "##### k3... #####"
+NGSadmix -likes $input -K 3 -P 10 -minMaf 0.05 -seed 1 -o $output\_k3
+
+echo "##### k4... #####"
+NGSadmix -likes $input -K 4 -P 10 -minMaf 0.05 -seed 1 -o $output\_k4
+
+echo "##### k5... #####"
+NGSadmix -likes $input -K 5 -P 10 -minMaf 0.05 -seed 1 -o $output\_k5
