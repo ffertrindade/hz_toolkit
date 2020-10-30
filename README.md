13f10ff6b64edbe5d152574e4b27b62b2c910ae3
ffertrindade <f.fertrindade@gmail.com>
2020-10-30 16:57:49 -0300

    Updated scripts NGSadmix and README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # hz_toolkit
 Scripts for hybrid zone genomic analyses
+
+angsd4aHMMinput.sh: Run angsd to generate maf file for the AIMs
+
+createInputAncestryHMM.py: This script estimates allele counts for two parental panels and hybrid individuals and geerates a file that can be used as input for aHMM
+Usage: createInputAncestryHMM.py mafPop1 nIndPop1 mafPop2 nIndPop2 bamlistHyb nIndHyb outFile
+
+getAIMs.sh: Create AIMs list based on a Fst threshold
+
+getSitesHzar_v2.sh: Perform cline analyses, this script runs hzar_par_v2.R
+
+hzar_par_v2.R: Perform cline analyses
+
+runNGSadmix.sh: Run NGSadmix for several k
+
+plotNGSadmix.R: Plot NGSadmix results
+Usage: plotNGSadmix.R qopt_file label_file k_num
+
+example/: Folder with example files
--- plotNGSadmix.R
+++ plotNGSadmix.R
@@ -1,41 +1,53 @@
-# plotar resultados NGSadmix (adaptado direto do site)
+# Plot NGSadmix results from *.qopt files
 
-setwd("")
+args <- commandArgs(T)
+if (length(args)!=3) {
+  stop("Usage: plotNGSadmix.R qopt_file label_file k_num", call.=FALSE)
+}
+
+qopt <- args[1]
+label <- args[2]
+k <- args[3]
+rm(args)
 
 # Get ID and pop info for each individual
-pop<-scan("poplabel_v4",what="theFuck")
-ind<-scan("indlabel_v4",what="theFuck")
+pop<-scan(label)
 
 # Read inferred admixture proportions file
-q<-read.table("batch001_run003_filtered_k5.qopt")
+q<-read.table(qopt)
 
 # Plot them (ordered by population or individual name)
 ord = order(pop)
-ordi = order(ind)
 
-png(filename="plots/run003_ld_ngsadmix_k5.png", width=900, height=300)
-par(mar=c(5,4,1,1))
-barplot(t(q)[,ordi],
-        col=c(4,5,8,1,3),
-        names=ind[ordi],
-        las=2,
-        ylab="Admixture proportions (k=5)",
-        main="run003 - pruned due to ld",
-        cex.axis=1, 
-        cex.names=1)
+filename <- paste(qopt, ".png", sep = "")
+y_title <- paste("Admixture proportions (k=", k, ")", sep = "")
+png(filename = filename, width = 900, height = 300)
+par(mar = c(5,4,1,1))
+
+barplot(t(q)[,ord],
+        col = c(4,5,8,1,3),
+        names = pop[ord],
+        las = 2,
+        ylab = y_title,
+        main = qopt,
+        cex.axis = 1, 
+        cex.names = 1)
+
 dev.off()
 
 # order according to population only
-png(filename="plots/batch001_run002_ngsadmix_k3_pop.png", width=500, height=300)
-par(mar=c(5,4,1,1))
+filename <- paste(qopt, "_b.png", sep = "")
+png(filename = filename, width = 500, height = 300)
+par(mar = c(5,4,1,1))
+
 barplot(t(q)[,ord],
-        col=c(4,5,8,1,3),
-        space=0,
-        border=NA,
-        xlab="Individuals from each population",
-        ylab="Admixture proportions (K=3)",
-        main="Batch001 - run002",
-        cex.axis=1)
-text(tapply(1:length(pop),pop[ord],mean),-0.05,unique(pop[ord]),xpd=T)
-abline(v=cumsum(sapply(unique(pop[ord]),function(x){sum(pop[ord]==x)})),col=1,lwd=1.2)
+        col = c(4,5,8,1,3),
+        space = 0,
+        border = NA,
+        xlab = "Individuals from each population",
+        ylab = y_title,
+        main = qopt,
+        cex.axis = 1)
+text(tapply(1:length(pop), pop[ord], mean), -0.05, unique(pop[ord]), xpd=T)
+abline(v = cumsum(sapply(unique(pop[ord]), function(x){sum(pop[ord]==x)})), col=1, lwd=1.2)
 dev.off()
